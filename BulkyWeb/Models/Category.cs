3af181ab1c6321f296da75e8e46e9ad0cb8cd874
Suryadeep <Suryadeep@DESKTOP-BF7IE6O>
2024-02-14 11:02:14 +0530

    Add project files.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public string? Name { get; set; }
+        [DisplayName("Category Order")]
+        [Range(1, 100, ErrorMessage = "Display order must be within 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
